d7d45c1ebee7477f8f0b19bbecba9f38a4545ee5
Julio Perdomo <jperdomocorrea@gmail.com>
2020-09-10 19:56:31 -0400

    First commit

---
+++ database/migrations/2020_09_10_234850_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('student_id');
+            $table->timestamp('lesson_date');
+            $table->string('subject');
+            $table->string('description');
+            $table->foreign('teacher_id')->references('id')->on('users');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
